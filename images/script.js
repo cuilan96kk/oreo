76094f53515c08ffd9221f808ff81897f1d5a0a8
Don Kim <dgkim5360@gmail.com>
2016-08-15 01:18:42 +0900

    initial commit

---
+++ images/script.js
@@ -0,0 +1,8 @@
+$(function() {
+	$("[href=#tab-header]").on("click", function() {
+		$("[href=#tab-search]").closest("li").removeClass("active");
+	});
+	$("[href=#tab-search]").on("shown.bs.tab", function() {
+		$("#tab-search").find("input").focus();
+	})
+});
